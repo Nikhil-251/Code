512dbf0b9f0f6f003020f01ff2c07fafc90e44e0
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-03-03 01:17:13 +0530

    Time: 1 ms (95.58%), Space: 51.4 MB (23.13%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int sum=nums[0];
         for(int i=1;i<nums.length;i++){
-            sum = sum^nums[i];
+            sum ^= nums[i];
         }
         return sum;
     }
