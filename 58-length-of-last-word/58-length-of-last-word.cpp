2c4db50423f4c2d01540adbc963a7d424faf4cbc
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-03-27 14:17:57 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (88.60%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        if(s == "a "){
-            return 1;
-        }
-        if(s.length()<=1){
-            return s.length();
-        }
+        
         int k = s.length()-1,count=0;
         while(k>=0){
             if(s[k]!=' '){
