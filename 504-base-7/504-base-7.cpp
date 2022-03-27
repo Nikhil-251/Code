1beb04dfaeb79930376473111849e78cb2bb8172
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-03-27 13:15:15 +0530

    Time: 5 ms (17.87%), Space: 6 MB (11.92%) - LeetHub

---
+++ 504-base-7/504-base-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string conv(int num){
+        string result="";
+        while(num>=7){
+            result = to_string(num%7)+ result;
+            num = num / 7;
+        }
+        if(num>0){
+            result = to_string(num%7)+ result;
+        }
+            
+          return result; 
+    }
+    string convertToBase7(int num) {
+        if(num==0)
+            return "0";
+        if(num<0){
+            return "-" + conv(-num);
+        }
+        return conv(num);
+    }
+};
