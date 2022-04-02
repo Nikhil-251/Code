8f90d486f6b9276fccbe9828df9d3ec3d4e376c3
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-03 03:27:37 +0530

    Time: 80 ms (30.75%), Space: 24.1 MB (19.18%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool validPalindrome(string s) {
         int l = 0 , r = s.length()-1;
-        while(l<=r){
+        while(l<r){
             if(s[l]!=s[r])
                 return ispal(s,l+1,r)||ispal(s,l,r-1);
             l++;
