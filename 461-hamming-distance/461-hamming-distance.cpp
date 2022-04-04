03705d0c6a6e6541e1e00dd543489a92caeae890
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-04 16:36:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.57%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res =0 ;
+        while(x>0||y>0){
+            res += (x%2)^(y%2);
+            x/=2;
+            y/=2;
+        }   
+        return res;
+    }
+};
