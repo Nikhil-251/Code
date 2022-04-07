afa3ef2f54dfb0f935c43d9e304101c508739134
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-08 01:48:48 +0530

    Time: 103 ms (37.55%), Space: 33 MB (33.60%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -9,7 +9,6 @@ public:
                 min=arr[i+1]-arr[i];
             }
         }
-        
         vector<vector<int>>ans;
         for(int i=0;i<n-1;i++){
             if((arr[i+1]-arr[i])==min){
