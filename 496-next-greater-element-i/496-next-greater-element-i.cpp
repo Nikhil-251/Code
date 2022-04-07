cc9fed1676fd9d5127254cb7236ade667fb56478
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-07 15:25:56 +0530

    Time: 10 ms (47.71%), Space: 9 MB (15.15%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> nextgreatestright(vector<int>nums){
+        stack<int>st;
+        vector<int>ans(nums.size());
+        for(int i=nums.size()-1;i>=0;i--){
+            while(st.size() > 0 && st.top()<nums[i]){
+                st.pop();
+            }
+            ans[i] = st.size()>0?st.top():-1;
+            st.push(nums[i]);
+        }
+        return ans;
+    }
+    
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ngr = nextgreatestright(nums2);
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<nums2.size();i++){
+            mp.insert(make_pair(nums2[i],ngr[i]));
+        }
+ 
+        vector<int>ans(nums1.size());
+        
+        for(int i =0; i<nums1.size();i++){
+            if(mp.find(nums1[i])==mp.end()){
+                ans[i]=-1;
+            }
+            else{
+                ans[i] = mp[nums1[i]];
+            }
+        }
+        return ans;
+    }
+};
