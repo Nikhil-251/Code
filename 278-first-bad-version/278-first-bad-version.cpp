1cc50a85ab14d85e813b8c4799dffdce13dbfeee
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-09 02:30:09 +0530

    Time: 5 ms (9.97%), Space: 5.7 MB (99.31%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,11 +4,13 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        for(int i=n;i>=0;i--){
-            if(!isBadVersion(i)){
-                return i+1;
-            };
+        int lower = 1, upper = n, mid;
+        while(lower < upper) {
+            mid = lower + (upper - lower) / 2;
+            if(!isBadVersion(mid)) 
+                lower = mid + 1;
+            else upper = mid;
         }
-        return 1;
+        return lower;
     }
 };
