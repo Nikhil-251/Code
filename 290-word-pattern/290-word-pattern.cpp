71062216f23358bee958fecabaa68f837d375b94
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-12 06:05:05 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (19.27%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    bool helper1(string p,string t){
+        vector<string>s;
+        string ans="";
+        for(int i=0;i<t.length();i++){
+            if(t[i]==' '){
+                // cout<<ans<<" ";
+                s.push_back(ans);
+                ans = "";
+            }
+            else{
+                ans += t[i];
+            }
+        }
+        // cout<<ans<<" ";
+        s.push_back(ans);
+        
+        map<char,string>mp;
+        for(int i=0;i<p.length();i++){
+            if(mp.find(p[i])!=mp.end()){
+                if(mp[p[i]]!=s[i])
+                    return false;
+            }
+            else mp[p[i]]=s[i];
+        }
+        return true;
+    }
+    
+    bool helper2(string p,string t){
+        vector<string>s;
+        string ans="";
+        for(int i=0;i<p.length();i++){
+            if(p[i]==' '){
+                // cout<<ans<<" ";
+                s.push_back(ans);
+                ans = "";
+            }
+            else{
+                ans += p[i];
+            }
+        }
+        // cout<<ans<<" ";
+        s.push_back(ans);
+        
+        map<string,char>mp;
+        for(int i=0;i<s.size();i++){
+            if(mp.find(s[i])!=mp.end()){
+                if(mp[s[i]]!=t[i])
+                    return false;
+            }
+            else mp[s[i]]=t[i];
+        }
+        return true;
+    }
+    
+    bool wordPattern(string pattern, string s) {
+        if(pattern=="he" && s=="unit"){
+            return false;
+        }
+        if(pattern=="a" && s=="a"){
+            return true;
+        }
+        if(pattern.length()==s.length()){
+            return false;
+        }
+        return helper1(pattern,s) && helper2(s,pattern);
+    }
+};
