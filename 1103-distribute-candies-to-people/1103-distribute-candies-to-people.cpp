f9cb5526b14f48d0e734500794d596583bda9920
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-14 06:44:59 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (21.41%) - LeetHub

---
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int npeople) {
+        vector<int>ans(npeople,0);
+        int i=0,n=1;
+        while(candies>0){
+            i = i%npeople;
+            ans[i]= ans[i]+min(candies,n);
+            candies-=n;
+            n++;
+            i++;
+        }
+        return ans;
+    }
+};
