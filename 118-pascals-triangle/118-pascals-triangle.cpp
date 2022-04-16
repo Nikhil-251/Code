5256e01e4afad92f0af3d0ac3a49a8741926a3ad
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-16 18:45:45 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.39%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,11 +4,9 @@ public:
         vector<vector<int>>p(row);
         for(int i=0;i<row;i++){
             p[i].resize(i+1,1);
-
             for(int j=1;j<i;j++){
                 p[i][j] = p[i-1][j]+p[i-1][j-1];
             }
-
         }
         return p;
     }
