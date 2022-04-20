a2371c84a5b978061326cbee95f4dc09a88589d8
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-04-20 07:13:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.89%) - LeetHub

--- 1185-day-of-the-week/1185-day-of-the-week.cpp
+++ 1185-day-of-the-week/1185-day-of-the-week.cpp
@@ -4,17 +4,17 @@ public:
         31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
         31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
     };
+    
     string weekName[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    bool isleapyear(int year)
-    {
+    
+    bool isleapyear(int year){
         return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
     }
-    int daystill1971(int year, int month, int day)
-    {
+    
+    int daystill1971(int year, int month, int day){
         int count = 0;
         int year1 = 1970, month1 = 1, day1 = 1;
-        while (year1 != year)
-        {
+        while (year1 != year){
             bool b = isleapyear(year1);
             if (b) count += 366;
             else count += 365;
@@ -25,6 +25,7 @@ public:
         count += day - 1;
         return count;
     }
+    
     string dayOfTheWeek(int day, int month, int year) {
         int days1 = daystill1971(2019, 9, 8);//today(2019,9,8) is Sunday
         int days2 = daystill1971(year, month, day);
