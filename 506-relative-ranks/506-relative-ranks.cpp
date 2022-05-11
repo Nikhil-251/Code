42b84a1829ec671f5faacb4f27ef653b4ce9d551
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-05-11 17:58:00 +0530

    Time: 13 ms (82.26%), Space: 10.2 MB (83.92%) - LeetHub

---
+++ 506-relative-ranks/506-relative-ranks.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<score.size();i++){
+            pq.push({score[i],i});
+        }
+        int i=0;
+        vector<string>v(pq.size());
+        while(!pq.empty()){
+            v[pq.top().second]=i==0?"Gold Medal":i==1?"Silver Medal":i==2?"Bronze Medal":to_string(i+1);
+            i+=1;
+            pq.pop();
+        }
+        return v;
+    }
+};
