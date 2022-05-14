7a4851088b384e6603cca3926ed0d32d98d92eff
Nikhil Gupta <77400458+Nikhil-251@users.noreply.github.com>
2022-05-14 18:18:23 +0530

    Time: 573 ms (14.50%), Space: 128.4 MB (8.20%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.cpp
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    bool isNiceSubstring(string& s, int i, int j) {
-        unordered_map<int, bool> mp;
-        bool isTrue = true;
-        for (int x = i; x <= j; x++) {
-            mp[s[x]] = true;
+    
+    bool isnice(string& s,int i,int j){
+        map<int,bool>mp;
+        for(int x=i;x<=j;x++){
+            mp[s[x]]=true;
         }
-        
-        while (i <= j) {
-            if (s[i] < 97) {
-                if (!mp[s[i] + 32]) {
+        while(i<=j){
+            if(s[i]<97){
+                if(!mp[s[i]+32])
                     return false;
                 }
-            } else {
-                if (!mp[s[i] - 32]) {
+            else{
+                if(!mp[s[i]-32]){
                     return false;
                 }
             }
             i++;
         }
-        
         return true;
     }
+    
     string longestNiceSubstring(string s) {
         int n = s.length();
-        int maxLength = 0;
-        string result;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isNiceSubstring(s, i, j)) {
-                    if (j - i + 1 > maxLength) {
-                        maxLength = j - i + 1;
-                        result = s.substr(i, j - i + 1);
+        int max = 0;
+        string res;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isnice(s,i,j)){
+                    if(j-i+1>max){
+                        max = j-i+1;
+                        res = s.substr(i,j-i+1);
                     }
                 }
             }
         }
-        
-        return result;
+        return res;
     }
 };
